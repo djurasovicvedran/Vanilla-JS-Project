228c54e8cadc3b80998e8dba9c875c691db1dc84
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-02-17 12:16:10 +0100

    Need to make fixed navbar

---
+++ Project 10 - Scroll/scroll.js
@@ -0,0 +1,48 @@
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// close links
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+navToggle.addEventListener('click', function(){
+    //linksContainer.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect();
+    const linksHeight = links.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
+});
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector('.top-link');
+
+
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageXOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+})
+// smooth scroll
+// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function(){
+    links.addEventListener('click', function(e){
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute('href');
+        console.log(id);
+
+    })
+})
