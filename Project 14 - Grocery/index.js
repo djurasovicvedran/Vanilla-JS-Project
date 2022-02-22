2b4bca3ae4317eb76053badd238974c2338a40f2
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-02-22 16:24:20 +0100

    Slider project finished, need to fix project 14

--- Project 14 - Grocery/index.js
+++ Project 14 - Grocery/index.js
@@ -19,11 +19,11 @@ clearBtn.addEventListener("click", clearItems);
 
 
 // functions
-function addItem (e){
+function addItem(e) {
     e.preventDefault();
     const value = grocery.value;
     const id = new Date().getTime().toString();
-    if (value && !editFlag ){
+    if (value && !editFlag) {
         const element = document.createElement("article");
         // add class
         element.classList.add("grocery-item");
@@ -43,10 +43,10 @@ function addItem (e){
           </button>
         </div>`
 
-const deleteBtn = element.querySelector("delete-btn");
-const editBtn = element.querySelector(".edit-btn");
-deleteBtn.addEventListener("click", deleteItem);
-editBtn.addEventListener("click", editItem);
+        const deleteBtn = element.querySelector("delete-btn");
+        const editBtn = element.querySelector(".edit-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        editBtn.addEventListener("click", editItem);
 
         // append child
         list.appendChild(element);
@@ -57,27 +57,31 @@ editBtn.addEventListener("click", editItem);
         // add to local storage
         addToLocalStorage(id, value);
         setBackToDefault();
-    } else if (value && editFlag ){
-        console.log("editing");
+    } else if (value && editFlag) {
+        editElement.innerHTML = value;
+        displayAlert("value changed", "success");
+        // edit local storage
+        editLocalStorage(editID, value);
+        setBackToDefault();
     } else {
         displayAlert("please enter value", "danger");
     }
 }
 // display alert
-function displayAlert (text, action){
+function displayAlert(text, action) {
     alert.textContent = text;
     alert.classList.add(`alert-${action}`);
 }
 // remove alert
-setTimeout(function(){
+setTimeout(function () {
     alert.textContent = "";
     alert.classList.remove(`alert-${action}`);
 }, 1000);
 // clear items
-function clearItems(){
+function clearItems() {
     const items = document.querySelectorAll("grocery-item");
-    if(items.length > 0){
-        items.forEach(function(item){
+    if (items.length > 0) {
+        items.forEach(function (item) {
             list.removeChild(item);
         })
     }
@@ -88,11 +92,11 @@ function clearItems(){
 }
 
 // delete function
-function deleteItem(e){
+function deleteItem(e) {
     const element = e.currentTarget.parentElement.parentElement;
     const id = element.dataset.id;
     list.removeChild(element);
-    if(list.children.length === 0){
+    if (list.children.length === 0) {
         container.classList.remove("show-container");
     }
     displayAlert("item removed", "danger");
@@ -103,26 +107,41 @@ function deleteItem(e){
     // removeFromLocalStorage(id);
 }
 // edit function
-function editItem (e) {
+function editItem(e) {
     const element = e.currentTarget.parentElement.parentElement;
     // set edit item
     editElement = e.currentTarget.parentElement.previousElementSibling;
     // set form value
-    grocery.value = editElement.innerHTML
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    submitBtn.textContent = "edit"
+        ;
 }
 // set back to default
-function setBackToDefault(){
-    grocery.value= "";
+function setBackToDefault() {
+    grocery.value = "";
     editFlag = false;
     editID = "";
     submitBtn.textContent = "submit";
 }
 
 // local storage
-function addToLocalStorage(id, value){
-    console.log("added to local storage");
+function addToLocalStorage(id, value) {
+    const grocery = {id, value};
 }
 function removeFromLocalStorage(id) {
 
 }
+function editLocalStorage(id, value) { }
 // setup items
+
+// localStorageAPI
+// setItem
+// getItem
+// removeItem
+// save as string
+
+// localStorage.setItem("orange", JSON.stringify["item1", "item2"]);
+// const oranges = JSON.parse(localStorage.getItem("orange"));
+// localStorage.removeItem("orange");
