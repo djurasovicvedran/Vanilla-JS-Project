5cf3a7ce0f4b44e5b2377560b60d991ee71c0492
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-02-25 12:23:34 +0100

    Edited footer, finished js functionality, need to fix mobile menu

--- First simple website/index.js
+++ First simple website/index.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+
 
 // display mobile menu
 const mobileMenu = () => {
@@ -7,3 +9,47 @@ const mobileMenu = () => {
     menuLinks.classList.toggle('active');
 };
 menu.addEventListener('click', mobileMenu);
+
+// show active menu when scrolling
+const highlightMenu = () => {
+    const element = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const services = document.querySelector('#services-page');
+    let scrollPosition = window.scrollY
+
+    // adds "highlight" class to my menu items
+    if(window.innerWidth > 960 && scrollPosition < 600){
+        homeMenu.classList.add("highlight");
+        aboutMenu.classList.remove("highlight");
+        return 
+    } else if (window.innerWidth > 960 && scrollPosition < 1400){
+        aboutMenu.classList.add("highlight");
+        homeMenu.classList.remove("highlight");
+        services.classList.remove("highlight");
+        return
+    }else if (window.innerWidth > 960 && scrollPosition < 2345){
+        aboutMenu.classList.remove("highlight");
+        services.classList.add("highlight");
+        return
+    }
+    if(element && window.innerWidth < 960 && scrollPosition <600 || element){
+        element.classList.remove("highlight");
+    }
+    
+
+}
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+// close mobile menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars){
+        menu.classList.toggle('.is-active');
+        menuLinks.classList.remove('active');
+
+    }
+}
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
